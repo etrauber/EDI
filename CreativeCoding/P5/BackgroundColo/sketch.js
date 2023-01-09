923636983f8db3ffa1a71980a21537cad69b91e0
etrauber <emma.trauber@gmail.com>
2023-01-09 12:42:32 -0500

    background color

---
+++ CreativeCoding/P5/BackgroundColo/sketch.js
@@ -0,0 +1,9 @@
+function setup() {
+  //creates the window width and height to be the size of my browser
+  createCanvas(windowWidth, windowHeight);
+  //create a red background
+  background(255,0,0);
+}
+
+function draw() {
+}
