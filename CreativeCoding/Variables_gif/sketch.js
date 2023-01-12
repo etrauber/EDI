eeb4daeaae528bf472094c60efdedd0226b3cf4c
etrauber <emma.trauber@gmail.com>
2023-01-12 08:12:54 -0500

    Update sketch.js

--- CreativeCoding/Variables_gif/sketch.js
+++ CreativeCoding/Variables_gif/sketch.js
@@ -13,12 +13,13 @@ function draw() {//runs in a loop
 
 var num = 30; //variable for the number of squares in the y direction
 translate(-150, -150);
+background('white');
+
 
 var sideLen = windowWidth/num; //variable for the side length of each square
 
   for(var y = 0; y < 2* windowWidth; y = y + sideLen){
   for(var x = 0; x < 2* windowWidth; x = x + sideLen) { //loop to create a row of squares in the x direction
-background('white');
   image(gif, x, y, windowWidth/num, windowWidth/num);
   }
   }
